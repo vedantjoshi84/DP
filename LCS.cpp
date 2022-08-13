6441f740fbba32a53490398ff6fff53685e65ece
Vedant Joshi <joshivedant84@gmail.com>
2022-08-13 10:10:53 +0530

    Eighth commit

---
+++ LCS.cpp
@@ -0,0 +1,31 @@
+// Problem Statement: https://leetcode.com/problems/longest-common-subsequence/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int t[m+1][n+1];
+        for(int i=0;i<m+1;i++) {
+            for(int j=0;j<n+1;j++) {
+                if(i==0 || j==0) {
+                    t[i][j] = 0;
+                }
+            }
+        }
+        for(int i=1;i<m+1;i++) {
+            for(int j=1;j<n+1;j++) {
+                if(text1[i-1] == text2[j-1]) {
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else {
+                    t[i][j] = max(t[i][j-1], t[i-1][j]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+};
